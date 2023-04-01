51800a931a52a9f808f129b409e07d750eafd2c9
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2023-04-01 17:09:29 -0300

    Questões 4 e 5

---
+++ Questao4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Distribuidora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definir o faturamento mensal de cada estado
+            decimal faturamentoSP = 67836.43m;
+            decimal faturamentoRJ = 36678.66m;
+            decimal faturamentoMG = 29229.88m;
+            decimal faturamentoES = 27165.48m;
+            decimal faturamentoOutros = 19849.53m;
+
+            // Calcular o faturamento total
+            decimal faturamentoTotal = faturamentoSP + faturamentoRJ + faturamentoMG + faturamentoES + faturamentoOutros;
+
+            // Calcular o percentual de representação de cada estado
+            decimal percentualSP = (faturamentoSP / faturamentoTotal) * 100;
+            decimal percentualRJ = (faturamentoRJ / faturamentoTotal) * 100;
+            decimal percentualMG = (faturamentoMG / faturamentoTotal) * 100;
+            decimal percentualES = (faturamentoES / faturamentoTotal) * 100;
+            decimal percentualOutros = (faturamentoOutros / faturamentoTotal) * 100;
+
+            // Exibir os resultados
+            Console.WriteLine($"Percentual de representação de SP: {percentualSP:f2}%");
+            Console.WriteLine($"Percentual de representação de RJ: {percentualRJ:f2}%");
+            Console.WriteLine($"Percentual de representação de MG: {percentualMG:f2}%");
+            Console.WriteLine($"Percentual de representação de ES: {percentualES:f2}%");
+            Console.WriteLine($"Percentual de representação de Outros: {percentualOutros:f2}%");
+        }
+    }
+}
---
+++ Questao5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Insira uma palavra:");
+        string str = Console.ReadLine();
+        char[] arr = str.ToCharArray();
+
+        for (int i = 0, j = arr.Length - 1; i < j; i++, j--) {
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        string reversed = new string(arr);
+        Console.WriteLine($"=====================================\n");
+        Console.WriteLine($"Palavra original:\n{str}");
+        Console.WriteLine($"Palavra invertida:\n{reversed}"); 
+    }
+}
