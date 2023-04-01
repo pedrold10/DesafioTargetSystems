1b5aca00d35fa52da2da3f7355ef328d6db531b8
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2023-04-01 17:00:28 -0300

    Questão 3

---
+++ Questao3/Faturamento.cs
@@ -0,0 +1,7 @@
+namespace Questao3{
+    class Faturamento
+    {
+        public int Dia { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
---
+++ Questao3/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Questao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lê o conteúdo do arquivo e armazena em uma string
+            string json = File.ReadAllText("./dados.json");
+
+            // Converte a string para uma lista de objetos Faturamento
+             var faturamentoMensal = JsonConvert.DeserializeObject<List<Faturamento>>(json);
+
+            var valores = new List<decimal>();
+            foreach (var faturamento in faturamentoMensal)
+            {
+                valores.Add(faturamento.Valor);
+            }
+
+            var media = valores.Where(x => x > 0).Average();
+            var diasAcimaDaMedia = valores.Count(x => x > media);
+            var menorValor = valores.Where(x => x > 0).Min();
+            var maiorValor = valores.Where(x => x > 0).Max();
+
+            Console.WriteLine($"Menor valor: R${menorValor:F2}");
+            Console.WriteLine($"Maior valor: R${maiorValor:F2}");
+            Console.WriteLine($"Número de dias acima da média: R${diasAcimaDaMedia:F2}");
+        }
+    }
+
+}
